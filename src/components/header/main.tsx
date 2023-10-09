32abf6b90da6a2f994967a82776bc9130fba5bff
junhyeok <junhyeok403@naver.com>
2023-10-09 18:30:14 +0900

    Feat: 질문 작성중

--- src/components/header/main.tsx
+++ src/components/header/main.tsx
@@ -1,6 +1,6 @@
 import styles from "../../style/main.module.css";
 import Explorer from "../../style/icon/explorer.svg";
-import { cs } from "../../services/frontend";
+import { cs, frontend, javascript } from "../../services/frontend";
 
 import ChevronDown from "../../style/icon/down.svg";
 import ChevronUp from "../../style/icon/up.svg";
@@ -10,15 +10,35 @@ interface FrontEndProps {
     id: number,
     title: string,
     arr: string,
-    isOpen: boolean
+    isOpen: boolean,
+    link: string,
+    link2: string
 }
 
 const MainPage = () => {
 
     const [one, setOne] = useState<FrontEndProps[]>(cs);
+    const [two, setTwo] = useState<FrontEndProps[]>(javascript);
+    const [three, setThree] = useState<FrontEndProps[]>(frontend);
 
     const one1 = (item: FrontEndProps) => {
         setOne(s => 
+            s.map(content =>
+                content.id === item.id ? { ...content, isOpen: !content.isOpen } : content
+            )
+        )
+    }
+
+    const one2 = (item: FrontEndProps) => {
+        setTwo(s => 
+            s.map(content =>
+                content.id === item.id ? { ...content, isOpen: !content.isOpen } : content
+            )
+        )
+    }
+
+    const one3 = (item: FrontEndProps) => {
+        setThree(s => 
             s.map(content =>
                 content.id === item.id ? { ...content, isOpen: !content.isOpen } : content
             )
@@ -38,6 +58,52 @@ const MainPage = () => {
                     {
                         one.length > 0 && one.map((item, index) => (
                             <li key={index} onClick={() => one1(item)}>
+                                <div className={styles.contents}>
+                                    {
+                                        item.isOpen ? <img src={ChevronUp} /> : <img src={ChevronDown} />
+                                    }
+                                    {item.title}
+                                </div>
+                                {
+                                    item.isOpen && <div className={styles.one___} dangerouslySetInnerHTML={{__html: item.arr}} />
+                                }
+                                
+                            </li>
+                        ))
+                    }
+                </ul>
+            </section>
+
+            <section>
+                <h2><img src={Explorer} /> Javascript</h2>
+
+                <ul>
+                    {
+                        two.length > 0 && two.map((item, index) => (
+                            <li key={index} onClick={() => one2(item)}>
+                                <div className={styles.contents}>
+                                    {
+                                        item.isOpen ? <img src={ChevronUp} /> : <img src={ChevronDown} />
+                                    }
+                                    {item.title}
+                                </div>
+                                {
+                                    item.isOpen && <div className={styles.one___} dangerouslySetInnerHTML={{__html: item.arr}} />
+                                }
+                                
+                            </li>
+                        ))
+                    }
+                </ul>
+            </section>
+
+            <section>
+                <h2><img src={Explorer} /> FrontEnd</h2>
+
+                <ul>
+                    {
+                        three.length > 0 && three.map((item, index) => (
+                            <li key={index} onClick={() => one3(item)}>
                                 <div className={styles.contents}>
                                     {
                                         item.isOpen ? <img src={ChevronUp} /> : <img src={ChevronDown} />
